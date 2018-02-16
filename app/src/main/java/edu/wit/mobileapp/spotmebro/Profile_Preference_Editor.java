8dc252a6161d3ca32297e31af163fad2115a7113
Yan Wen Huang <yanwenhuang.95@gmail.com>
2018-02-16 10:21:13 -0500

    Added Profile_Preference_Editor

---
+++ app/src/main/java/edu/wit/mobileapp/spotmebro/Profile_Preference_Editor.java
@@ -0,0 +1,57 @@
+package edu.wit.mobileapp.spotmebro;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ViewSwitcher;
+
+public class Profile_Preference_Editor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__preference__editor);
+
+        //Returns back to the Main Page
+        Button back_btn = (Button)findViewById(R.id.back);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile_Preference_Editor.this, Main_Page.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rightArrow = (ImageButton)findViewById(R.id.right_arrow);
+        ImageButton leftArrow = (ImageButton)findViewById(R.id.left_arrow);
+
+        final ViewSwitcher switchView = (ViewSwitcher)findViewById(R.id.profile_preference_flipper);
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+        switchView.setInAnimation(in);
+        switchView.setOutAnimation(out);
+
+        rightArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchView.showNext();
+            }
+        });
+
+        //Flips the view back to Profile Editor
+        leftArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchView.showNext();
+            }
+        });
+    }
+}
